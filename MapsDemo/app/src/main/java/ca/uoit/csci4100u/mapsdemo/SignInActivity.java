12bebcdd1818b43cfafafb8686cf5ade9294b9bb
bradgardiner <bradgardiner21@gmail.com>
2017-12-10 15:45:50 -0500

    Addinig Sign in and sign up skeletons

---
+++ MapsDemo/app/src/main/java/ca/uoit/csci4100u/mapsdemo/SignInActivity.java
@@ -0,0 +1,27 @@
+package ca.uoit.csci4100u.mapsdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * Created by bradg on 2017-12-10.
+ */
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+
+
+    }
+
+    public void launch_sign_up(View view){
+
+    }
+
+}
---
+++ MapsDemo/app/src/main/java/ca/uoit/csci4100u/mapsdemo/SignUpActivity.java
@@ -0,0 +1,21 @@
+package ca.uoit.csci4100u.mapsdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bradg on 2017-12-10.
+ */
+
+public class SignUpActivity extends AppCompatActivity{
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+
+    }
+
+}
